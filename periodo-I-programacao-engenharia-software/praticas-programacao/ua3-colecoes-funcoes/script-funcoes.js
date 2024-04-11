b3f28aa984b1eda423f39585e191e8c29607e3e8
Erick Ferreira da Silva <erick.silva@jfpe.jus.br>
2024-04-11 11:45:04 -0300

    Periodo I, praticas de programação - 11/04/2024

--- periodo-I-programacao-engenharia-software/praticas-programacao/ua3-colecoes-funcoes/script-funcoes.js
+++ periodo-I-programacao-engenharia-software/praticas-programacao/ua3-colecoes-funcoes/script-funcoes.js
@@ -1,7 +1,8 @@
-// Função de declaração
+// Função principal
 function calcular(operacao) {
     const n1 = document.getElementById('n1').value;
     const n2 = document.getElementById('n2').value;
+    let n2Zero = 'Não é possível dividir um número com 0';
     let resultado = document.getElementById('resultado');
 
     switch (operacao) {
@@ -19,17 +20,35 @@ function calcular(operacao) {
             break;
 
         case '/':
-            resultado.innerHTML = dividir(n1, n2);
+
+            if (n2 == 0) {
+                console.error(n2Zero);
+                resultado.innerHTML = n2Zero;
+            }
+            else {
+                resultado.innerHTML = dividir(n1, n2);
+            }
+
             break;
 
         case '%':
-            resultado.innerHTML = modulo(n1, n2);
+
+            if (n2 == 0) {
+                console.error(n2Zero);
+                resultado.innerHTML = n2Zero;
+            }
+            else {
+                resultado.innerHTML = modulo(n1, n2);
+            }
+
             break;
     }
 }
 
 calcular();
 
+
+// Funções auxiliares
 function somar(n1, n2) {
     return Number(n1) + Number(n2);
 }
---
+++ periodo-I-programacao-engenharia-software/praticas-programacao/ua3-colecoes-funcoes/script-recursividade.js
@@ -0,0 +1,13 @@
+// Função com recursividade: fatorial
+function fatorial(x) {
+
+    if(x < 1) {
+        return 1;
+    }
+    else {
+        return x * fatorial(x-1);
+    }
+    
+}
+
+console.log(fatorial(10));
